4dd632c056c874586ddced28e667d3cb026a3bbc
Priyamvada <priyamvadakaliraman156@gmail.com>
2026-07-31 21:05:54 +0530

    test: add placeholder Jest tests for CI

---
+++ tests/agents.test.js
@@ -0,0 +1,5 @@
+describe("Agents", () => {
+  test("placeholder test", () => {
+    expect(true).toBe(true);
+  });
+});
---
+++ tests/pipeline.test.js
@@ -0,0 +1,5 @@
+describe("Pipeline", () => {
+  test("placeholder test", () => {
+    expect(true).toBe(true);
+  });
+});
